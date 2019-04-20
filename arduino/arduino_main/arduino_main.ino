7d6a3f39882755cdea460604ec4754971113d94e
Matias Brignone <mnbrignone@gmail.com>
2019-04-20 17:39:31 -0300

    add arduino program

---
+++ arduino/arduino_main/arduino_main.ino
@@ -0,0 +1,26 @@
+
+int rx_byte;
+
+void setup() {
+  // start serial port at 115200 bps:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    rx_byte = Serial.read();
+    switch(rx_byte) {
+      case '1':
+        Serial.print("E_1");
+        break;
+      case '2':
+        Serial.print("Got 2");
+      default:
+        if (rx_byte != '\n' && rx_byte != '\r') {
+          Serial.print("Got Unknown");
+        }
+    } // switch
+  } // if serial available
+} // loop
