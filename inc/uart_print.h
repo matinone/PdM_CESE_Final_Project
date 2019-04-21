b5d1280590ed4ccc7a1fd7121c9ad251c8db411b
Matias Brignone <mnbrignone@gmail.com>
2019-04-21 13:05:11 -0300

    add comments to uart_print header

--- inc/uart_print.h
+++ inc/uart_print.h
@@ -17,9 +17,58 @@
 
 
 /* ===== Prototypes of public functions ===== */
+
+/*------------------------------------------------------------------
+|  Function: clear_screen
+| ------------------------------------------------------------------
+|  Description: sends the required characters to clear the console 
+				screen. 
+|
+|  Parameters:
+|		- 
+|
+|  Returns:  void
+*-------------------------------------------------------------------*/
 void clear_screen();
+
+
+/*------------------------------------------------------------------
+|  Function: clear_screen
+| ------------------------------------------------------------------
+|  Description: sends a welcome message through the UART. 
+|
+|  Parameters:
+|		- 
+|
+|  Returns:  void
+*-------------------------------------------------------------------*/
 void print_welcome_msg();
+
+
+/*------------------------------------------------------------------
+|  Function: clear_screen
+| ------------------------------------------------------------------
+|  Description: prints a help message, showing the possible commands
+				and a short description. 
+|
+|  Parameters:
+|		- 
+|
+|  Returns:  void
+*-------------------------------------------------------------------*/
 void print_help();
+
+
+/*------------------------------------------------------------------
+|  Function: clear_screen
+| ------------------------------------------------------------------
+|  Description: prints the value read from the ADC.
+|
+|  Parameters:
+|		adc_value - value to print
+|
+|  Returns:  void
+*-------------------------------------------------------------------*/
 void print_adc_value(uint16_t adc_value);
 
 
